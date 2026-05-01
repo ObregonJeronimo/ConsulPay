e795bba7e78a5d9820d08317eb9c9da34055cfb1
ThiagoJoelP <thiagojoel17@hotmail.com>
2026-05-01 16:29:13 -0300

    Delete api/super/eliminar-profesional.js

--- api/super/eliminar-profesional.js
+++
@@ -1,8 +0,0 @@
-// Este archivo fue movido a /api/super.js (router unico) por el limite
-// de 12 funciones serverless del plan Hobby de Vercel. Lo dejamos vacio
-// (sin export default) para que Vercel no lo detecte como funcion.
-//
-// Si necesitas la logica, esta en /api/super.js bajo la accion
-// 'eliminar-profesional'.
-//
-// TODO: borrar este archivo manualmente del repo en algun momento.
